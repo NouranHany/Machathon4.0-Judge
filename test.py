7420c094844bc3203c5996396d792245c55be14a
Kareem Elsawah <alsawahkareem@gmail.com>
2023-02-11 19:00:58 +0200

    Cleaned up test code

--- test.py
+++ test.py
@@ -1,30 +1,34 @@
-from PIL import Image
+"""
+Example code using the judge module
+"""
+# pylint: disable=import-error
 import cv2
+import keyboard
+
 from machathon_judge import Simulator, Judge
 
-import keyboard
-import time
-import matplotlib.pyplot as plt
 
-count = 0
-start = None
+def run_car(simulator: Simulator) -> None:
+    """
+    Function to control the car using keyboard
 
-
-def run_car(simulator: Simulator):
-    global count, start
-    if start is None:
-        start = time.monotonic()
-    else:
-        total = time.monotonic()
-        print(count / (total - start))
-        if count > 100:
-            count = 0
-            start = None
-    count += 1
+    Parameters
+    ----------
+    simulator : Simulator
+        The simulator object to control the car
+        The only functions that should be used are:
+        - get_image()
+        - set_car_steering()
+        - set_car_velocity()
+        - get_state()
+    """
+    # Get the image and show it
     img = simulator.get_image()
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     cv2.imshow("image", img)
     cv2.waitKey(1)
+
+    # Control the car using keyboard
     steering = 0
     if keyboard.is_pressed("a"):
         steering = 1
@@ -37,10 +41,14 @@ def run_car(simulator: Simulator):
     elif keyboard.is_pressed("s"):
         throttle = -1
 
-    simulator.move_car(throttle * 15, steering * simulator.max_steer_angle / 1.7)
+    simulator.set_car_steering(steering * simulator.max_steer_angle / 1.7)
+    simulator.set_car_velocity(throttle * 25)
 
 
 if __name__ == "__main__":
+    # Initialize any variables needed
     cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+
+    # Start the judge and simulation
     judge = Judge(run_car)
     judge.run(send_score=True)
